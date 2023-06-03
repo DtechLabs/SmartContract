219787a14763e7cbb49ba40ddbd98d3156e0b787
Yuri Drigin <yuri.drigin@icloud.com>
2023-06-03 12:41:10 +0400

    Add gitignore

---
+++ Sources/SmartContract/Codable/ABIRawTypeParser.swift
@@ -0,0 +1,16 @@
+//
+//  ABIRawTypeCoder.swift
+//  
+//
+//  Created by Yuri on 31.05.2023.
+//
+
+import Foundation
+
+enum ABIRawTypeParser {
+    
+    static func parse(_ rawValue: String) throws -> ABIRawType {
+        
+    }
+    
+}
---
+++ Sources/SmartContract/Extensions/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yuri on 31.05.2023.
+//
+
+import Foundation
